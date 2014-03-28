d04d7b4fac7a0894084e6e5e8f0cc1dc9548e3cd
kiangkuang <kiangkuang@gmail.com>
2014-03-28 18:04:21 +0800

    Assignment 2

---
+++ Assignment2/js/main.js
@@ -0,0 +1,78 @@
+$(function() {
+	$.UISearch({articleID:'#main',placeholder:'Search'});
+
+	$("#alarm-view").on("singletap", function() {
+		$.UIGoToArticle("#alarm-stops");
+	});
+	
+	$("#map-view").on("singletap", function() {
+		$.UIGoToArticle("#main");
+	});
+	
+	$("#sound").on("singletap", function() {
+		$.UIGoToArticle("#customize");
+	});
+	
+	$("#setringtone").on("singletap", function() {
+		$.UIGoToArticle("#ringtone");
+	});
+	
+	
+	$('.segmented').UIPanelToggle('#toggle-panels', function(){});
+	
+	$('#stopslist').slideToggle();
+	$('#alarmSwitch1').on('singletap swipeleft swiperight', function() {
+		if (this.classList.contains('on')) {
+			$('#stopslist').slideToggle();
+		} else {
+			$('#stopslist').slideToggle();
+		}
+	}); 
+	
+	$('#distlist').slideToggle();
+	$('#alarmSwitch2').on('singletap swipeleft swiperight', function() {
+		if (this.classList.contains('on')) {
+			$('#distlist').slideToggle();
+		} else {
+			$('#distlist').slideToggle();
+		}
+	}); 
+	
+	$('#stops').UIStepper({ 
+		start: 1,
+		end: 8,
+		defaultValue: 2
+	});
+
+	$('#rangeValue1').html($('#rangeControl1').val()+"m");
+	$('#rangeControl1').on('change', function() {
+		$('#rangeValue1').html($(this).val()+"m");
+	});
+
+	$('.list.select').UISelectList({
+		selected: 0,
+		callback: function() {
+			$("#select-response").html($(this).text()); // display text
+		}
+	}); 
+	
+	$("#openPopup").on("singletap", function() {
+		console.log("lol");
+		var mess = "Enable Stops alarm or Distance alarm to test!";
+		if ($('#alarmSwitch1').hasClass('on')) {
+			mess = 'Alight in '+ $(".stepper label").html() + ' stops!';
+		} 
+		if ($('#alarmSwitch2').hasClass('on')) {
+			mess = 'Alight in '+ $('#rangeValue1').html()+'!';
+		}
+		$.UIPopup({
+			id: "Alarm",
+			title: 'Attention!', 
+			message: mess, 
+			continueButton: 'Dismiss', 
+			callback: function() {
+			}
+		});
+	});
+
+});
