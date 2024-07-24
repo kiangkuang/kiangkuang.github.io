82c0037d41d43d6ef49dac058b80684007d4ec54
Kiang Kuang <kiangkuang@gmail.com>
2024-07-25 04:33:17 +0800

    update jquery

--- TwitchPlaysPokemon/js/script.js
+++ TwitchPlaysPokemon/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(e) {
 	$('.with-hover-text, .regular-link').click(function(e){
 		e.stopPropagation();
 	});
-	
+
 	/***************
 	* = Hover text *
 	* Hover text for the last slide
@@ -41,10 +41,10 @@ $(document).ready(function(e) {
 				);
 		}
 	);
-	
+
 	var img_loaded = 0;
 	var j_images = [];
-	
+
 	/*************************
 	* = Controls active menu *
 	* Hover text for the last slide
@@ -53,26 +53,26 @@ $(document).ready(function(e) {
 		var time = new Date().getTime();
 		var oldHref = $(this).attr('src');
 		var myImg = $('<img />').attr('src', oldHref + '?' + time );
-		
-		myImg.load(function(e) {
+
+		myImg.on('load', function(e) {
 			img_loaded += 1;;
 			if ( img_loaded == $('#slide-3 img').length ) {
 				$(function() {
-					
+
 					var pause = 10;
 					$(document).scroll(function(e) {
 						delay(function() {
-							
+
 							var tops = [];
-							
+
 							$('.story').each(function(index, element) {
 								tops.push( $(element).offset().top - 200 );
 							});
-				
+
 							var scroll_top = $(this).scrollTop();
-							
+
 							var lis = $('.nav > li');
-							
+
 							for ( var i=tops.length-1; i>=0; i-- ) {
 								if ( scroll_top >= tops[i] ) {
 									menu_focus( lis[i], i+1 );
@@ -87,7 +87,7 @@ $(document).ready(function(e) {
 			}
 		});
 	});
-	
+
 });
 
 /******************
@@ -98,27 +98,27 @@ $(function() {
 	$(window).resize(function() {
 		delay(function() {
 				var gallery_images = $('#slide-3 img');
-				
+
 				var images_per_row = 0;
 				if ( gallery_images.length % 2 == 0 ) {
 					images_per_row = gallery_images.length / 2;
 				} else {
 					images_per_row = gallery_images.length / 2 + 1;
 				}
-				
+
 				var gallery_width = $('#slide-3 img').width() * $('#slide-3 img').length;
 				gallery_width /= 2;
 				if ( $('#slide-3 img').length % 2 != 0 ) {
 					gallery_width += $('#slide-3 img').width();
 				}
-				
+
 				$('#slide-3 .row').css('width', gallery_width );
-				
+
 				var left_pos = $('#slide-3 .row').width() - $('body').width();
 				left_pos /= -2;
-				
+
 				$('#slide-3 .row').css('left', left_pos);
-			
+
 			},
 			pause
 		);
@@ -143,22 +143,22 @@ function menu_focus( element, i ) {
 			return;
 		}
 	}
-	
+
 	enable_arrows( i );
-		
+
 	if ( i == 1 || i == 6 )
 		$('.navbar').removeClass('inv');
 	else
 		$('.navbar').addClass('inv');
-	
+
 	$('.nav > li').removeClass('active');
 	$(element).addClass('active');
-	
+
 	var icon = $(element).find('.icon');
-	
+
 	var left_pos = icon.offset().left - $('.nav').offset().left;
 	var el_width = icon.width() + $(element).find('.text').width() + 10;
-	
+
 	$('.active-menu').stop(false, false).animate(
 		{
 			left: left_pos,
@@ -193,17 +193,17 @@ jQuery(document).ready(function ($) {
 	button = $('.button');
 	mywindow = $(window);
 	htmlbody = $('html,body');
-	
+
 	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
 	//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
 	function goToByScroll(dataslide) {
 		var offset_top = ( dataslide == 1 ) ? '0px' : $('.slide[data-slide="' + dataslide + '"]').offset().top;
-		
+
 		htmlbody.stop(false, false).animate({
 			scrollTop: offset_top
 		}, 1500, 'easeInOutQuart');
 	}
-	
+
 	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
 	links.click(function (e) {
 		e.preventDefault();
@@ -211,7 +211,7 @@ jQuery(document).ready(function ($) {
 		goToByScroll(dataslide);
 		$(".nav-collapse").collapse('hide');
 	});
-	
+
 	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
 	$('.navigation-slide').click(function (e) {
 		e.preventDefault();
@@ -227,19 +227,19 @@ jQuery(document).ready(function ($) {
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var menu_item = $('.nav').find('li');
-	
+
 	menu_item.hover(
 		function(e) {
 			var icon = $(this).find('.icon');
-			
+
 			var left_pos = icon.offset().left - $('.nav').offset().left;
 			var el_width = icon.width() + $(this).find('.text').width() + 10;
-			
+
 			var hover_bar = $('<div class="active-menu special-active-menu"></div>')
 				.css('left', left_pos)
 				.css('width', el_width)
 				.attr('id', 'special-active-menu-' + $(this).data('slide') );
-			
+
 			$('.active-menu').after( hover_bar );
 		},
 		function(e) {
@@ -254,7 +254,7 @@ jQuery(document).ready(function ($) {
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var images = $('#slide-3 a');
-	
+
 	images.hover(
 		function(e) {
 			var asta = $(this).find('img');
@@ -290,19 +290,19 @@ jQuery(document).ready(function ($) {
 jQuery(document).ready(function ($) {
 	//Cache some variables
 	var arrows = $('#arrows div');
-	
+
 	arrows.click(function(e) {
 		e.preventDefault();
-		
+
 		if ( $(this).hasClass('disabled') )
 			return;
-		
+
 		var slide = null;
 		var datasheet = $('.nav > li.active').data('slide');
 		var offset_top = false;
 		var offset_left = false;
-		
-		
+
+
 		switch( $(this).attr('id') ) {
 			case 'arrow-up':
 				offset_top = ( datasheet - 1 == 1 ) ? '0px' : $('.slide[data-slide="' + (datasheet-1) + '"]').offset().top;
@@ -323,13 +323,13 @@ jQuery(document).ready(function ($) {
 				}
 				break;
 		}
-		
+
 		if ( offset_top != false ) {
 			htmlbody.stop(false, false).animate({
 				scrollTop: offset_top
 			}, 1500, 'easeInOutQuart');
 		}
-		
+
 		if ( offset_left != false ) {
 			if ( $('#slide-3 .row').width() != $('body').width() ) {
 				$('#slide-3 .row').stop(false, false).animate({
@@ -380,7 +380,7 @@ $("#submitBtn").on("click", function(){
 		$("#labelName").fadeToggle(400);
 		retard = false;
 	}
-	
+
 	if ($("#inputEmail").val().length <= 0) {
 		$("#labelEmail").fadeToggle(400,"",function(){
 			$("#labelEmail").html("DON'T YOU HAVE AN EMAIL?");
@@ -400,7 +400,7 @@ $("#submitBtn").on("click", function(){
 		$("#labelEmail").fadeToggle(400);
 		hobo = false;
 	}
-	
+
 	if ($("#inputFeedback").val().length <= 0) {
 		$("#labelFeedback").fadeToggle(400,"",function(){
 			$("#labelFeedback").html("WHAT DO YOU SEEK FROM THE HELIX?");
@@ -414,7 +414,7 @@ $("#submitBtn").on("click", function(){
 		$("#labelFeedback").fadeToggle(400);
 		dome = false;
 	}
-	
+
 	if ($("#inputName").val().length > 0 && $("#inputEmail").val().length > 0 && $("#inputFeedback").val().length > 0 && IsEmail($("#inputEmail").val())) {
 		$("#slideout").slideToggle();
 		$("#thanks").fadeToggle();
